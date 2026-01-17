a6926921239f6ea59d9e44af0e5719efe5714750
Thomas <thomasbustos@lets-talk-ai.com>
2026-01-17 19:17:06 +0100

    added reposityroy in innit repo for weekly digest

--- src/app/repositories/__init__.py
+++ src/app/repositories/__init__.py
@@ -3,11 +3,13 @@
 from app.repositories.channel_repository import ChannelRepository
 from app.repositories.video_repository import VideoRepository
 from app.repositories.daily_digest_repository import DailyDigestRepository
+from app.repositories.weekly_digest_repository import WeeklyDigestRepository
 
 __all__ = [
     "ChannelRepository",
     "VideoRepository",
     "DailyDigestRepository",
+    "WeeklyDigestRepository",
 ]
 
 
